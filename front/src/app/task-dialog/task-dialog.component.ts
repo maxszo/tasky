dcbf53d9b689d395ca85bdd662ec2f5be19d78c4
Max SZOSTKIEWICZ <mszostkiewicz@consultants-solutec.fr>
2024-12-04 12:06:41 +0100

    fix/repair creation and update task forms and add error message

--- front/src/app/task-dialog/task-dialog.component.ts
+++ front/src/app/task-dialog/task-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit  } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -15,13 +15,41 @@ import { CommonModule } from '@angular/common'; // Import CommonModule
   templateUrl: './task-dialog.component.html',
   styleUrls: ['./task-dialog.component.css'],
 })
-export class TaskDialogComponent {
+export class TaskDialogComponent implements OnInit {
+  errorMessage: string | null = null;
+
+
   constructor(
     public dialogRef: MatDialogRef<TaskDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
+  ngOnInit() {
+    // Ensure the assigned user is correctly prefilled in the update dialog
+    if (this.data.task.users) {
+      const assignedUser = this.data.users.find(
+        (user: any) => user.id === this.data.task.users.id
+      );
+      this.data.task.users = assignedUser || null;
+    }
+  }
+
   onSave() {
+    // Reset error message
+    this.errorMessage = null;
+
+    // Validate that required fields are not empty
+    if (
+      !this.data.task.name ||
+      !this.data.task.userStoryNumber ||
+      !this.data.task.description ||
+      !this.data.task.priority ||
+      !this.data.task.state
+    ) {
+      this.errorMessage = 'Please fill in all required fields.';
+      return;
+    }
+
     this.dialogRef.close(this.data.task);
   }
 
